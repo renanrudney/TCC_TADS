4cd559b4881c8fd92153dd0d93df1976b5c5a4fe
Renan Rudney <renanrudney@gmail.com>
2024-11-14 01:50:54 -0300

    feat: add filter resultados by date and type
    fix: date timezone

--- server/app/controllers/resultados_controller.rb
+++ server/app/controllers/resultados_controller.rb
@@ -1,7 +1,7 @@
 class ResultadosController < ApplicationController
   def index
-    resultados = Resultado::List.call(usuario_id: @usuario_atual.id)
+    resultados = Resultado::List.call(usuario_id: @usuario_atual.id, date: params[:date], type: params[:type])
     @pagy, @records = pagy_array(resultados)
-    render json: { records: @records, meta: pagy_metadata(@pagy)}, status: :ok
+    render json: { records: @records, meta: pagy_metadata(@pagy) }, status: :ok
   end
 end
--- server/app/models/resultado/list.rb
+++ server/app/models/resultado/list.rb
@@ -21,29 +21,54 @@ class Resultado::List
         when :heel_rise
           "/heel_rise_resultados/#{id}"
         end
-      [{
+      [ {
         "rel": "self",
         "href": url
-      }]
+      } ]
     end
   end
 
-  def self.call(usuario_id:)
-    resultados = []
-    ::HitpointResultado.where(usuario_id: ).each do |hp|
-      attributes = hp.attributes.merge(type: :hitpoint)
-      resultados << Item.new(attributes)
+  def self.call(usuario_id:, date:, type:)
+    format_date = date.present? ? date.in_time_zone("America/Sao_Paulo") : nil
+    @resultados = []
+    if type.blank?
+      updownarm(usuario_id:, realizado: format_date)
+      heelrise(usuario_id:, realizado: format_date)
+      hitpoint(usuario_id:, realizado: format_date)
+    else
+      case type.to_sym
+      when :up_down_arm
+        updownarm(usuario_id:, realizado: format_date)
+      when :heel_rise
+        heelrise(usuario_id:, realizado: format_date)
+      when :hitpoint
+        hitpoint(usuario_id:, realizado: format_date)
+      end
     end
-    ::UpDownArmResultado.where(usuario_id:).each do |ud|
+
+    @resultados
+  end
+
+  def self.hitpoint(usuario_id:, realizado:)
+    ::HitpointResultado.where(usuario_id:).where(realizado: realizado&.beginning_of_day..realizado&.end_of_day).find_each do |hp|
+      attributes = hp.attributes.merge(type: :hitpoint)
+      @resultados << Item.new(attributes)
+    end
+  end
+
+  def self.updownarm(usuario_id:, realizado:)
+    ::UpDownArmResultado.where(usuario_id:).where(realizado: realizado&.beginning_of_day..realizado&.end_of_day).find_each do |ud|
       attributes = ud.attributes.merge(type: :up_down_arm)
       attributes = attributes.merge(accelerometers: ud.accelerometers, gyroscopes: ud.gyroscopes)
-      resultados << Item.new(attributes)
+      @resultados << Item.new(attributes)
     end
-    ::HeelRiseResultado.where(usuario_id:).each do |ud|
-      attributes = ud.attributes.merge(type: :heel_rise)
-      attributes = attributes.merge(accelerometers: ud.accelerometers, gyroscopes: ud.gyroscopes)
-      resultados << Item.new(attributes)
+  end
+
+  def self.heelrise(usuario_id:, realizado:)
+    ::HeelRiseResultado.where(usuario_id:).where(realizado: realizado&.beginning_of_day..realizado&.end_of_day).find_each do |hr|
+      attributes = hr.attributes.merge(type: :heel_rise)
+      attributes = attributes.merge(accelerometers: hr.accelerometers, gyroscopes: hr.gyroscopes)
+      @resultados << Item.new(attributes)
     end
-    resultados
   end
 end
