39a7f358ae84b3d8671809a2b7b270b03bfafe5a
Renan Rudney <renanrudney@gmail.com>
2024-11-27 22:10:26 -0300

    fix: accelerometers and gyroscopes persistence

--- server/app/models/heel_rise_resultado.rb
+++ server/app/models/heel_rise_resultado.rb
@@ -5,22 +5,4 @@ class HeelRiseResultado < ApplicationRecord
   belongs_to :usuario, class_name: "Usuario::Base"
 
   accepts_nested_attributes_for :accelerometers, :gyroscopes
-
-  private
-
-  def autosave_associated_records_for_accelerometers
-    return if accelerometers.empty?
-
-    Accelerometer.insert_all(
-      accelerometers.map { |a| a.attributes.slice(:x_axis, :y_axis, :z_axis, :timestamp) }, record_timestamps: true
-    )
-  end
-
-  def autosave_associated_records_for_gyroscopes
-    return if gyroscopes.empty?
-
-    Gyroscope.insert_all(
-      gyroscopes.map { |a| a.attributes.slice(:x_axis, :y_axis, :z_axis, :timestamp) }, record_timestamps: true
-    )
-  end
 end
