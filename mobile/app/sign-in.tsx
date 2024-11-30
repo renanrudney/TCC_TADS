0da7bb097f54f634952f6de49a405d61de1db5b0
Renan Rudney <renanrudney@gmail.com>
2024-11-30 00:27:16 -0300

    feat: add login page

--- mobile/app/sign-in.tsx
+++ mobile/app/sign-in.tsx
@@ -1,38 +1,102 @@
-import { useState } from 'react';
-import { Text, TextInput, View } from 'react-native';
+import { Text, TextInput, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { router } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useForm, Controller } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 import { serverAPI } from '@/api/serverApi';
 import { useSession } from '@/src/ctx';
 import { ToastError } from '@/components/ToastError';
 
 export default function SignIn() {
+  return (
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ width: '100%', height: '50%', flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{ fontSize: 42, textAlign: 'center' }}>Testes{'\n'}Parkinson</Text>
+      </View>
+      <View style={{ backgroundColor: '#145a73', width: '100%', flex: 1 }}>
+        <LoginForm />
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const schema = yup.object().shape({
+  login: yup.string().required('Login não pode ficar em branco.'),
+  senha: yup.string().required('Senha não pode ficar em branco.'),
+});
+
+const LoginForm = () => {
   const { signIn } = useSession();
-  const [login, setLogin] = useState('')
-  const [password, setPassword] = useState('')
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data: { login: string, senha: string }) => {
+    serverAPI.post('/login', { login: data.login, senha: data.senha })
+      .then(res => {
+        signIn(res.data.token)
+        router.replace('/')
+      })
+      .catch(err => ToastError(err.response.data))
+  };
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <TextInput
-        placeholder='Login'
-        onChangeText={setLogin}
-      />
-      <TextInput
-        placeholder='Password'
-        onChangeText={setPassword}
-        secureTextEntry={true}
-      />
-      <Text
-        onPress={() => {
-          serverAPI.post('/login', { login: login, senha: password })
-            .then(res => {
-              signIn(res.data.token)
-              router.replace('/')
-            })
-            .catch(err => ToastError(err.response.data))
-        }}>
-        Sign In
-      </Text>
+    <View style={{ margin: 48, gap: 36 }}>
+      <View>
+        <Text style={styles.inputLabel}>Login</Text>
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              placeholder="Login"
+              style={styles.input}
+            />
+          )}
+          name="login"
+        />
+        {errors.login && <Text style={styles.error}>{errors.login.message}</Text>}
+      </View>
+      <View>
+        <Text style={styles.inputLabel}>Senha</Text>
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              secureTextEntry
+              placeholder="Senha"
+              style={styles.input}
+            />
+          )}
+          name="senha"
+        />
+        {errors.senha && <Text style={styles.error}>{errors.senha.message}</Text>}
+      </View>
+      <TouchableOpacity onPress={handleSubmit(onSubmit)} style={styles.button}>
+        <Text style={styles.buttonText}>Entrar</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  inputLabel: { color: 'white', fontWeight: 'bold' },
+  input: { backgroundColor: 'white', padding: 6 },
+  error: { color: 'red', fontWeight: 'bold' },
+  button: {
+    padding: 4,
+    width: 160,
+    backgroundColor: 'white',
+    color: '#145a73',
+    borderRadius: 50,
+    alignSelf: 'center',
+  },
+  buttonText: { textAlign: 'center', fontSize: 24, fontWeight: 'bold'}
+});
