b28f51c0b45e4a6f995916371262c745d591faeb
Renan Rudney <renanrudney@gmail.com>
2024-12-03 01:20:26 -0300

    feat: add highcharts

--- frontend/src/app/pages/resultados/view/view-resultado/view-resultado.component.ts
+++ frontend/src/app/pages/resultados/view/view-resultado/view-resultado.component.ts
@@ -1,14 +1,25 @@
 import { Component, inject, OnInit } from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
+import { ActivatedRoute } from "@angular/router";
 import { ResultadoProviderService } from "../../../../services/providers/resultado/resultado-provider.service";
+import { HighchartsChartModule } from 'highcharts-angular'
+import Highcharts from 'highcharts'
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-view-resultado',
-  imports: [],
+  imports: [CommonModule, HighchartsChartModule],
   templateUrl: './view-resultado.component.html',
   styleUrl: './view-resultado.component.css'
 })
 export class ViewResultadoComponent implements OnInit {
+  Highcharts: typeof Highcharts = Highcharts;
+  accelerometerChartOptions: Highcharts.Options = {}
+  gyroscopeChartOptions: Highcharts.Options = {}
+  hitpointChartOptions: Highcharts.Options = {}
+  loadAccelerometer = false
+  loadGyroscope = false
+  loadHitpoint = false
+  gyroscopes: any
   resultadoId: any;
   tipo: any;
   resultado: any;
@@ -24,14 +35,85 @@ export class ViewResultadoComponent implements OnInit {
 
   getResultadoByIdTipo() {       
     this.httpProvider.getResultadoByIdTipo(this.resultadoId, this.tipo).subscribe((data : any) => {
-      console.log(data)
-      if (data != null && data.body != null) {
-        var resultData = data.body;
+      if (data != null) {
+        var resultData = data;
         if (resultData) {
           this.resultado = resultData;
+          console.log(this.resultado)
+
+          if (this.tipo === 'hitpoint') {
+            this.buildHitpointChartData()
+          } else {
+            this.buildAccelerometerChartData()
+            this.buildGyroscopeChartData()
+          }
         }
       }
     },
     (error :any)=> { }); 
+  }
+
+  buildHitpointChartData() {
+    this.hitpointChartOptions =
+    {
+      title: { text: 'Toque/Hit' },
+      series : [
+        {
+          data: this.resultado.hit_data.map((h: any) => [parseFloat(h.timestamp), h.hit_number]),
+          type: 'line',
+        },
+      ]
+    }
+    console.log(this.hitpointChartOptions)
+    this.loadHitpoint = true
+  }
+
+  buildAccelerometerChartData() {
+    this.accelerometerChartOptions =
+    {
+      title: { text: 'Acelerômetro' },
+      series: [
+        {
+          data: this.resultado.accelerometers.map((a: any) => [parseFloat(a.timestamp), parseFloat(a.x_axis)]),
+          type: 'line',
+          name: 'x_axis'
+        },
+        {
+          data: this.resultado.accelerometers.map((a: any) => [parseFloat(a.timestamp), parseFloat(a.y_axis)]),
+          type: 'line',
+          name: 'y_axis'
+        },
+        {
+          data: this.resultado.accelerometers.map((a: any) => [parseFloat(a.timestamp), parseFloat(a.z_axis)]),
+          type: 'line',
+          name: 'z_axis'
+        },
+      ]
+    }
+    this.loadAccelerometer = true
+  }
+
+  buildGyroscopeChartData() {
+    this.gyroscopeChartOptions = {
+      title: { text: 'Giroscópio' },
+      series: [
+        {
+          data: this.resultado.gyroscopes.map((g: any) => [parseFloat(g.timestamp), parseFloat(g.x_axis)]),
+          type: 'line',
+          name: 'x_axis'
+        },
+        {
+          data: this.resultado.gyroscopes.map((g: any) => [parseFloat(g.timestamp), parseFloat(g.y_axis)]),
+          type: 'line',
+          name: 'y_axis'
+        },
+        {
+          data: this.resultado.gyroscopes.map((g: any) => [parseFloat(g.timestamp), parseFloat(g.z_axis)]),
+          type: 'line',
+          name: 'z_axis'
+        },
+      ]
+    }
+    this.loadGyroscope = true
   }
 }
