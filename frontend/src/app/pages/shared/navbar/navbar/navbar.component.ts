4eb220415bd4a96b2de2a945f171ba846180e1ff
Renan Rudney <renanrudney@gmail.com>
2024-12-03 16:41:45 -0300

    fix: active items

--- frontend/src/app/pages/shared/navbar/navbar/navbar.component.ts
+++ frontend/src/app/pages/shared/navbar/navbar/navbar.component.ts
@@ -2,10 +2,11 @@ import { Component, inject, OnInit } from '@angular/core';
 import { AuthService } from '../../../../auth/auth.service';
 import { CommonModule } from '@angular/common';
 import { BehaviorSubject, Observable, of, Subject } from 'rxjs';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
@@ -13,9 +14,16 @@ export class NavbarComponent implements OnInit {
   authService: AuthService  = inject(AuthService)
   isLoggedIn: Observable<boolean> = this.authService.isLoggedIn$;
 
-  constructor() {}
+  public activePath = '';
+
+  constructor(private router: Router) {}
 
   ngOnInit() {
     this.isLoggedIn = this.authService.isLoggedIn$;
+  }
+
+  onSelectPath(newPath: string) {
+    this.activePath = newPath;
+    console.log(this.activePath)
   }
 }
