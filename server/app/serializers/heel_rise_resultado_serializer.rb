b84898d96578fc6d4f3aa93909620d39c044987d
Renan Rudney <renanrudney@gmail.com>
2024-12-04 00:33:54 -0300

    fix: test list with usuario

--- server/app/serializers/heel_rise_resultado_serializer.rb
+++ server/app/serializers/heel_rise_resultado_serializer.rb
@@ -1,7 +1,7 @@
 class HeelRiseResultadoSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :id, :realizado, :accelerometers, :gyroscopes, :usuario_id, :created_at, :updated_at, :links
+  attributes :id, :realizado, :accelerometers, :gyroscopes, :usuario, :created_at, :updated_at, :links
 
   def links
     [
