d846c40790dab51e92213a7d06b2a4381545a19a
Renan Rudney <renanrudney@gmail.com>
2024-12-10 10:44:14 -0300

    fix: profissionais serializer

--- server/app/serializers/profissional_serializer.rb
+++ server/app/serializers/profissional_serializer.rb
@@ -1,7 +1,7 @@
 class ProfissionalSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :cpf, :login, :tipo, :nome, :sobrenome, :nascimento, :tipo_registro, :registro, :especialidade, :links
+  attributes :id, :cpf, :login, :tipo, :nome, :sobrenome, :nascimento, :tipo_registro, :registro, :especialidade, :links
 
   def tipo_registro = object.profissional.tipo_registro
 
